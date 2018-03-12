e26edf182aba585c9ca63f4fe7fb634ba273b979
caixiangqi <caixiangqi@gomo.com>
2018-03-12 11:15:45 +0800

    update register

--- pwa/register.js
+++ pwa/register.js
@@ -34,51 +34,53 @@ function subscribe () {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('./sw.js')
       .then(function (registration) {
-        return registration.pushManager.getSubscription()
-          .then(function(subscription) {
-            // if subscription exit
-            if (subscription) {
-              return
-            }
-            // if user unsubscribe
-            if (getStorageItem('vue_pwa_notification_disabled')) {
-              return
-            }
-            // subscribe
-            return registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(vapidPublicKey)
-              })
-              .then(function (subscription) {
-                var rawKey = subscription.getKey ? subscription.getKey('p256dh') : ''
-                var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : ''
-                key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : ''
-                authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : ''
-                endpoint = subscription.endpoint
-                // post user's subscription to server
-                return fetch(NOTIFICATION_HOST + '/api/notification/subscribe', {
-                    method: 'post',
-                    headers: new Headers({
-                      'content-type': 'application/json'
-                    }),
-                    body: JSON.stringify({
-                      endpoint: endpoint,
-                      key: key,
-                      authSecret: authSecret
-                    })
-                  })
-                  .then(function (res) {
-                    res.json()
-                      .then(json => {
-                        // save subscription user_id in localstorage for unsubscribe
-                        setStorageItem('vue_pwa_user_id', json.user_id)
+        if (serviceWorkerRegistration.pushManager) {
+          return registration.pushManager.getSubscription()
+            .then(function(subscription) {
+              // if subscription exit
+              if (subscription) {
+                return
+              }
+              // if user unsubscribe
+              if (getStorageItem('vue_pwa_notification_disabled')) {
+                return
+              }
+              // subscribe
+              return registration.pushManager.subscribe({
+                  userVisibleOnly: true,
+                  applicationServerKey: urlBase64ToUint8Array(vapidPublicKey)
+                })
+                .then(function (subscription) {
+                  var rawKey = subscription.getKey ? subscription.getKey('p256dh') : ''
+                  var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : ''
+                  key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : ''
+                  authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : ''
+                  endpoint = subscription.endpoint
+                  // post user's subscription to server
+                  return fetch(NOTIFICATION_HOST + '/api/notification/subscribe', {
+                      method: 'post',
+                      headers: new Headers({
+                        'content-type': 'application/json'
+                      }),
+                      body: JSON.stringify({
+                        endpoint: endpoint,
+                        key: key,
+                        authSecret: authSecret
                       })
-                  })
-                  .catch(function (err) {
-                    console.log(err)
-                  })
-              })
-          })
+                    })
+                    .then(function (res) {
+                      res.json()
+                        .then(json => {
+                          // save subscription user_id in localstorage for unsubscribe
+                          setStorageItem('vue_pwa_user_id', json.user_id)
+                        })
+                    })
+                    .catch(function (err) {
+                      console.log(err)
+                    })
+                })
+            })
+        }
       })
       .catch(function (err) {
         console.log('ServiceWorker registration failed: ', err, err.code, err.message, err.name)
@@ -90,36 +92,29 @@ function unsubscribe() {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.ready
       .then((serviceWorkerRegistration) => {
-        serviceWorkerRegistration.pushManager.getSubscription()
-          .then((subscription) => {
-            // if subscription don't exit
-            if (!subscription) {
-              return
-            }
-            // save unsubscribe status in localstorage
-            setStorageItem('vue_pwa_notification_disabled', '1')
-            subscription.unsubscribe()
-              .then(function () {
-                var vuePwaUserId = getStorageItem('vue_pwa_user_id')
-                if (vuePwaUserId) {
-                  // remove subscription user_id in localstorage
-                  removeStorageItem('vue_pwa_user_id')
-                  // delete user's subscription on server
-                  // return fetch(NOTIFICATION_HOST + '/api/notification/unsubscribe', {
-                  //   method: 'delete',
-                  //   headers: new Headers({
-                  //     'content-type': 'application/json'
-                  //   }),
-                  //   body: JSON.stringify({
-                  //     user_id: vuePwaUserId
-                  //   })
-                  // })
-                }
-              })
-              .catch((e) => {
-                logger.error('Error thrown while unsubscribing from push messaging', e)
-              })
-          })
+        if (serviceWorkerRegistration.pushManager) {
+          serviceWorkerRegistration.pushManager.getSubscription()
+            .then((subscription) => {
+              // if subscription don't exit
+              if (!subscription) {
+                return
+              }
+              // save unsubscribe status in localstorage
+              setStorageItem('vue_pwa_notification_disabled', '1')
+              subscription.unsubscribe()
+                .then(function () {
+                  var vuePwaUserId = getStorageItem('vue_pwa_user_id')
+                  if (vuePwaUserId) {
+                    // remove subscription user_id in localstorage
+                    removeStorageItem('vue_pwa_user_id')
+                    // todo
+                  }
+                })
+                .catch((e) => {
+                  logger.error('Error thrown while unsubscribing from push messaging', e)
+                })
+            })
+          }
       })
   }
 }
